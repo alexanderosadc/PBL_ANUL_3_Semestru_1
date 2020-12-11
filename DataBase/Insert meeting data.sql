d4122306939bfc7d564797601a5b78b5873e9e21
Maxim Volosenco <maxim.volosenco@extendaretail.com>
2020-12-11 14:15:37 +0200

    stuf

--- DataBase/Insert meeting data.sql
+++ DataBase/Insert meeting data.sql
@@ -1,19 +1,32 @@
 USE PBLSecurity;
 GO
 DECLARE @meetingTitle nvarchar(255) = 'test insert';
+DECLARE @startTime nvarchar(255) = '2020-01-01 00:00:00';
+DECLARE @endTime nvarchar(255) = '2020-12-01 00:00:00';
+DECLARE @creatorID int = 4;
+DECLARE @roomID int = 3;
+DECLARE @meetingStatusID int = 2;
+
+DECLARE @meetingID int = 5;
+DECLARE @userID int = 1;
+DECLARE @email nvarchar(255) = 'alex@utm.md'
 
 
---SELECT TOP (1000) 
---      ,[meetingTitle]
---      ,[startTime]
---      ,[endTime]
---      ,[creatorID]
---      ,[meetingStatusID]
---      ,[roomID]
---  FROM [PBLSecurity].[dbo].[Meeting]
-
+-- insert info about meeting
 --INSERT INTO [Meeting] (meetingTitle, startTime, endTime, creatorID, meetingStatusID, roomID)
 --VALUES 
---	('Empty')
---	('Created'),
---	('Deleted')
+--	(@meetingTitle, @startTime, @endTime, @creatorID, @meetingStatusID, @roomID)
+
+--extract meetingID for created meeting
+--SELECT meetingID
+--FROM Meeting
+--WHERE meetingTitle = @meetingTitle
+
+--SELECT email
+--FROM [Authentication]
+--WHERE email = @email;
+
+-- insert attendees
+--INSERT INTO Attendees(meetingID, userID)
+--VALUES
+	--(@meetingID, @userID)
---
+++ DataBase/auth.sql
@@ -0,0 +1,31 @@
+
+DECLARE @email nvarchar(255) = 'notalex@utm.md'
+DECLARE @token nvarchar(255) = 'sdasfdsbnwfdsfsd'
+--DECLARE @email nvarchar(255) = 'alex@utm.md'
+DECLARE @name nvarchar(255) = 'notAlex'
+DECLARE @isAdmin int = 0
+DECLARE @company nvarchar(255) = 'utm.md'
+DECLARE @userID int = 7
+DECLARE @auhProvider nvarchar(255) = 'random stuff'
+
+
+--SELECT email
+--FROM [Authentication]
+--WHERE email = @email
+
+--INSERT INTO [User] ([name], isAdmin, company)
+--VALUES
+--	(@name, @isAdmin, @company)
+
+--SELECT userID
+--FROM [User]
+--WHERE [name] = @name AND company = @company
+
+
+
+INSERT INTO [Authentication] (email, authProvider,token, userID)
+VALUES
+	(@email, @auhProvider, @token, @userID)
+
+
+
